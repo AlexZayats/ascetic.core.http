f0e5fe994a8b917a2930532c69115f3a72f17123
Alex Zayats <a.zayats@outlook.com>
2021-02-08 19:50:22 +0300

    initial commit

---
+++ Extensions/HttpClientExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ascetic.Core.Http.Extensions
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<TResult> GetJsonAsync<TResult>(this HttpClient httpClient, string requestUri, CancellationToken cancellationToken = default)
+        {
+            var response = await httpClient.GetAsync(requestUri, cancellationToken);
+            return await response.ReadFromJsonAsync<TResult>(cancellationToken);
+        }
+
+        public static Task PostJsonAsync<TData>(this HttpClient httpClient, string requestUri, TData data, CancellationToken cancellationToken = default)
+        {
+            return httpClient.PostAsync(requestUri, GetJsonHttpContent(data), cancellationToken);
+        }
+
+        public static async Task<TResult> PostJsonAsync<TResult, TData>(this HttpClient httpClient, string requestUri, TData data, CancellationToken cancellationToken = default)
+        {
+            var response = await httpClient.PostAsync(requestUri, GetJsonHttpContent(data), cancellationToken);
+            return await response.ReadFromJsonAsync<TResult>(cancellationToken);
+        }
+
+        public static Task PutJsonAsync<TData>(this HttpClient httpClient, string requestUri, TData data, CancellationToken cancellationToken = default)
+        {
+            return httpClient.PutAsync(requestUri, GetJsonHttpContent(data), cancellationToken);
+        }
+
+        public static async Task<TResult> PutJsonAsync<TResult, TData>(this HttpClient httpClient, string requestUri, TData data, CancellationToken cancellationToken = default)
+        {
+            var response = await httpClient.PutAsync(requestUri, GetJsonHttpContent(data), cancellationToken);
+            return await response.ReadFromJsonAsync<TResult>(cancellationToken);
+        }
+
+        public static Task DeleteJsonAsync(this HttpClient httpClient, string requestUri, CancellationToken cancellationToken = default)
+        {
+            return httpClient.DeleteAsync(requestUri, cancellationToken);
+        }
+
+        private static async Task<TResult> ReadFromJsonAsync<TResult>(this HttpResponseMessage response, CancellationToken cancellationToken = default)
+        {
+            var contentStream = await response.Content.ReadAsStreamAsync();
+            return await JsonSerializer.DeserializeAsync<TResult>(contentStream, new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            }, cancellationToken);
+        }
+
+        private static HttpContent GetJsonHttpContent<TData>(TData data)
+        {
+            var dataJson = JsonSerializer.Serialize(data, new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            });
+            return new StringContent(dataJson, Encoding.UTF8, "application/json");
+        }
+    }
+}
---
+++ Extensions/UriBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Ascetic.Core.Http.Extensions
+{
+    public static class UriBuilderExtensions
+    {
+        public static string AddOptionalParameter(this string uri, string name, object value)
+        {
+            if (value != null)
+            {
+                return AddParameter(uri, name, value);
+            }
+            return uri;
+        }
+
+        public static string AddParameter(this string uri, string name, object value)
+        {
+            uri += (uri.Contains("?") ? "&" : "?") + $"{name}={value}";
+            return uri;
+        }
+    }
+}
